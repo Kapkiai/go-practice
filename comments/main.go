67d91ca628eac6d200ac5c447c30acb344236849
Mathew Kapkiai <mathewkiprop17@gmail.com>
2022-04-28 12:11:31 +0300

    value literals

--- comments/main.go
+++ comments/main.go
@@ -21,7 +21,7 @@ func StatRandomNumbers(n int) (int, int) {
 	// A for loop control flow
 	for i := 0; i < n; i++ {
 		// An if-else control flow
-		if rand.Intn(MaxRnd) < MaxRnd/2 {
+		if rand.Intn(MaxRnd) <= MaxRnd/2 {
 			a = a + 1
 		} else {
 			b++ // same as b = b + 1
@@ -31,13 +31,35 @@ func StatRandomNumbers(n int) (int, int) {
 	return a, b // this function returns two results
 }
 
+func runeValLiteral() {
+	//Unicode value of character a is 97
+	println('a' == 97)
+	// '\' must be followed by exactly three octal digits to represent a byte
+	println('a' == '\141')
+	// '\x' must be followed by exactly two hex digits to represent a byte value
+	println('a' == '\x61')
+	// '\u' must be followed by exactly four hex digits to represent a rune value
+	println('a' == '\u0061')
+	// '\U' must be followed by exactly eight hex digits to represent a rune value
+	// Each such octal or hex digit sequence must represent a legal Unicode code point, otherwise, it fails to compile
+	println('a' == '\U00000061')
+	println(0x61 == '\x61')
+	println('\u4f17' == '众')
+
+	println("\a")
+}
+
 // main function is the entry point of a program
 func main() {
-	var num = 100
+	var num = 49
 	// Call the declared StatRandom Number
 	x, y := StatRandomNumbers(num)
 
 	// Call two built-in functions (print and println).
 	print("Result: ", x, " + ", y, " = ", num, "? ")
 	println(x+y == num)
+
+	// Calling runeLiteral
+
+	runeValLiteral()
 }
